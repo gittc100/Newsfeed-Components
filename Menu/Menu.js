7c8c61dff4a649218211057a2c432035a27152e6
Thomas Claydon <tomclaydon102@gmail.com>
2018-10-24 16:57:21 -0400

    Second Submission - Stretch Task 1 - Menu

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,12 +1,38 @@
+let count = 2;
 
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence. 
-  menu.classList.toggle('menu--open');
+  
+  if(count%2 === 0){
+    count++;
+    TweenLite.to(".menu", 3, { 
+      ease: Elastic.easeOut.config(1, 0.3), 
+      left:0
+    });
+  }else if(count%2 !== 0){
+    count++;
+    TweenLite.to(".menu", 3, { 
+      ease: Elastic.easeOut.config(1, 0.3), 
+      left:-350
+    });
+  }
+  // menu.classList.toggle('menu--open');
+  console.log(count);
+}
+
+const toggleWithBody = () => {
+    TweenLite.to(".menu", 3, { 
+      ease: Elastic.easeOut.config(1, 0.3), 
+      left:-350
+    });
 }
 
 // Start Here: Create a reference to the ".menu" class
 const menu = document.querySelector('.menu');
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelector('.menu-button');
+const articleButton = document.querySelector('.articles');
 // Using your menuButton reference, add a click handler that calls toggleMenu
 menuButton.addEventListener('click', toggleMenu);
+articleButton.addEventListener('click', toggleWithBody);
+
